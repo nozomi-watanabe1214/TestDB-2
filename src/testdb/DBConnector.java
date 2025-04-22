ca19b2adc0a21d2e951956332a6d9f68181c9167
渡辺のぞみ <shogo.kk@icloud.com>
2025-04-22 21:47:49 +0900

    DataBaseConnection #Practice Test.java作成＆ドキュメンテーションコメント追加

--- src/testdb/DBConnector.java
+++ src/testdb/DBConnector.java
@@ -8,22 +8,30 @@ public class DBConnector {
 	private static String driverName = "com.mysql.jdbc.Driver";
 	private static String url =
 	"jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
+	//mysql用のURL指定の仕方、?以降は必須ではない
 
 	private static String user = "root";
 	private static String password = "";
+	//指定したアカウントのパスワード（今回はなし）
 
 	public Connection getConnection() {
+		//接続状態へ
 
 		Connection con = null;
+		//一度、状態を初期化
 
 		try{
 			Class.forName(driverName);
 			con = DriverManager.getConnection(url,user,password);
-			} catch (ClassNotFoundException e) {
+			//ドライバーをロードして使えるように
+			}
+		catch (ClassNotFoundException e) {
 			e.printStackTrace() ;
-			} catch (SQLException e) {
-
-				e.printStackTrace() ;
+			//クラスが見つからない際の例外設定
+			}
+		catch (SQLException e) {
+			e.printStackTrace() ;
+			//データベース処理に関する例外設定
 			}
 			return con ;
 			}
